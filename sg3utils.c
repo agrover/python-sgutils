0cb0cd10a128289996119df2f4c47135eb7a45f2
Andy Grover <agrover@redhat.com>
2013-01-14 16:29:06 -0800

    More progress
    Use library fd open/close routines
    
    Preface function names with tags to prevent collisions
    
    Fix BuildValue strings
    
    Implement inquiry
    
    Implement sense
    
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- sg3utils.c
+++ sg3utils.c
@@ -37,30 +37,7 @@
 #define __STDC_FORMAT_MACROS 1
 #include <inttypes.h>
 
-/* Returns >= 0 if successful, otherwise returns negated errno. */
-static int open_device(const char *device_name)
-{
-	int fd;
-	int oflags = O_NONBLOCK | O_RDONLY;
-
-	fd = open(device_name, oflags);
-	if (fd < 0)
-		fd = -errno;
-	return fd;
-}
-
-/* Returns 0 if successful. If error in Unix returns negated errno. */
-int close_device(int device_fd)
-{
-	int res;
-
-	res = close(device_fd);
-	if (res < 0)
-		res = -errno;
-	return res;
-}
-
-#define RESP_BUFF_LEN 32
+#define READCAP_RESP_BUFF_LEN 32
 
 static char readcap_doc[] =
 "Returns the result of READ CAPACITY(16), as a tuple.\n"
@@ -69,20 +46,20 @@ static char readcap_doc[] =
 " p_i_exponent, lbppbe, lbpme, lbprz, lalba)";
 
 static PyObject *
-readcap(PyObject *self, PyObject *args)
+sbc_readcap(PyObject *self, PyObject *args)
 {
 	int sg_fd;
 	const char *sg_name;
 	int ret;
 	uint64_t llast_blk_addr, block_size;
-	unsigned char resp_buff[RESP_BUFF_LEN];
+	unsigned char resp_buff[READCAP_RESP_BUFF_LEN];
 	int k, prot_en, p_type, lbpme, lbprz, lbppbe, lalba, p_i_exponent;
 
 	if (!PyArg_ParseTuple(args, "s", &sg_name)) {
 		return NULL;
 	}
 
-	sg_fd = open_device(sg_name);
+	sg_fd = sg_cmds_open_device(sg_name, 1, 0);
 	if (sg_fd < 0) {
 		PyErr_SetString(PyExc_IOError, "Could not open device");
 		return NULL;
@@ -90,14 +67,14 @@ readcap(PyObject *self, PyObject *args)
 
 	memset(resp_buff, 0, sizeof(resp_buff));
 
-	ret = sg_ll_readcap_16(sg_fd, 0, 0, &resp_buff, RESP_BUFF_LEN, 0, 0);
+	ret = sg_ll_readcap_16(sg_fd, 0, 0, &resp_buff, sizeof(resp_buff), 0, 0);
 	if (ret < 0) {
 		PyErr_SetString(PyExc_IOError, "SCSI command failed");
-		close_device(sg_fd);
+		sg_cmds_close_device(sg_fd);
 		return NULL;
 	}
 
-	close_device(sg_fd);
+	sg_cmds_close_device(sg_fd);
 
 	for (k = 0, llast_blk_addr = 0; k < 8; ++k) {
 		llast_blk_addr <<= 8;
@@ -113,17 +90,166 @@ readcap(PyObject *self, PyObject *args)
 	lbprz = !!(resp_buff[14] & 0x40);
 	lalba = ((resp_buff[14] & 0x3f) << 8) + resp_buff[15];
 
-	return Py_BuildValue("(kkkkkkkkk)", llast_blk_addr, block_size,
+	return Py_BuildValue("(kkbbbbbbb)", llast_blk_addr, block_size,
 			     p_type, prot_en, p_i_exponent, lbppbe, lbpme,
 			     lbprz, lalba);
 }
 
+static char inquiry_doc[] =
+"Returns the result of INQUIRY, as a tuple.\n"
+"See SCSI SPC-3 spec for more info:\n"
+"(vendor, product, revision, peripheral_qualifier, peripheral_type,\n"
+" rmb, version, NormACA, HiSup, response_data_format, sccs, acc,\n"
+" tpgs, 3pc, protect, BQue, EncServ, MultiP, MChngr, Addr16, WBus16,\n"
+" sync, linked, CmdQue)";
+
+static PyObject *
+spc_inquiry(PyObject *self, PyObject *args)
+{
+	int sg_fd;
+	const char *sg_name;
+	int ret;
+	struct sg_simple_inquiry_resp inq_data;
+
+	if (!PyArg_ParseTuple(args, "s", &sg_name)) {
+		return NULL;
+	}
+
+	sg_fd = sg_cmds_open_device(sg_name, 1, 0);
+	if (sg_fd < 0) {
+		PyErr_SetString(PyExc_IOError, "Could not open device");
+		return NULL;
+	}
+
+	memset(&inq_data, 0, sizeof(inq_data));
+
+	ret = sg_simple_inquiry(sg_fd, &inq_data, 0, 0);
+	if (ret < 0) {
+		PyErr_SetString(PyExc_IOError, "SCSI command failed");
+		sg_cmds_close_device(sg_fd);
+		return NULL;
+	}
+
+	sg_cmds_close_device(sg_fd);
+
+	return Py_BuildValue("(sssbbbbbbbbbbbbbbbbbbbbb)", inq_data.vendor,
+			     inq_data.product, inq_data.revision,
+			     inq_data.peripheral_qualifier,
+			     inq_data.peripheral_type, inq_data.rmb,
+			     inq_data.version,
+			     ((inq_data.byte_3 >> 5) & 0x1),	/* NormACA */
+			     ((inq_data.byte_3 >> 4) & 0x1),	/* HiSup */
+			     (inq_data.byte_3 & 0xF),		/* response data format */
+			     ((inq_data.byte_5 >> 7) & 0x1),	/* sccs */
+			     ((inq_data.byte_5 >> 6) & 0x1),	/* acc */
+			     ((inq_data.byte_5 >> 5) & 0x3),	/* tpgs */
+			     ((inq_data.byte_5 >> 3) & 0x1),	/* 3pc */
+			     (inq_data.byte_5 & 0x1),		/* protect */
+			     ((inq_data.byte_6 >> 7) & 0x1),	/* BQue */
+			     ((inq_data.byte_6 >> 6) & 0x1),	/* EncServ */
+			     ((inq_data.byte_6 >> 4) & 0x1),	/* MultiP */
+			     ((inq_data.byte_6 >> 3) & 0x1),	/* MChngr */
+			     (inq_data.byte_6 & 0x1),		/* Addr16 */
+			     ((inq_data.byte_7 >> 5) & 0x1),	/* WBus16 */
+			     ((inq_data.byte_7 >> 4) & 0x1),	/* sync */
+			     ((inq_data.byte_7 >> 3) & 0x1),	/* linked */
+			     ((inq_data.byte_7 >> 1) & 0x1)	/* CmdQue */
+		);
+}
+
+static char sense_doc[] =
+"Returns the result of MODE SENSE(10), as a dictionary of sense mode codes\n"
+" to page code data.\n"
+"See SCSI SPC-3 spec for more info.";
+
+#define PG_CODE_MAX 0x3f
+
+static PyObject *
+spc_sense(PyObject *self, PyObject *args)
+{
+	int sg_fd;
+	const char *sg_name;
+	int ret;
+	unsigned char rbuf[4096];
+	int i;
+	unsigned char *ucp;
+	unsigned int mode_data_len, block_desc_len;
+	PyObject *dict;
+
+	if (!PyArg_ParseTuple(args, "s", &sg_name)) {
+		return NULL;
+	}
+
+	sg_fd = sg_cmds_open_device(sg_name, 1, 0);
+	if (sg_fd < 0) {
+		PyErr_SetString(PyExc_IOError, "Could not open device");
+		return NULL;
+	}
+
+	memset(&rbuf, 0, sizeof(rbuf));
+
+	ret = sg_ll_mode_sense10(sg_fd, 0, 0, 0, 0x3f, 0, rbuf, sizeof(rbuf),
+				 0, 0);
+	if (ret < 0) {
+		PyErr_SetString(PyExc_IOError, "SCSI command failed");
+		sg_cmds_close_device(sg_fd);
+		return NULL;
+	}
+
+	sg_cmds_close_device(sg_fd);
+
+	block_desc_len = (rbuf[6] << 8) + rbuf[7];
+	/* rest_of_header_len = 6 */
+	mode_data_len = (rbuf[0] << 8) + rbuf[1] - 6 - block_desc_len;
+	/* 8 byte hdr, then block descriptors, THEN mode data */
+	ucp = rbuf + 8 + block_desc_len;
+
+	dict = PyDict_New();
+	if (!dict)
+		return NULL;
+
+	/* Build a dict of sense mode page code to sense data */
+	for (i = 0; mode_data_len > 0; i++) {
+		PyObject *name, *value;
+		int mp_len = ucp[1] + 2;
+
+		name = PyInt_FromLong(ucp[0]);
+		if (!name)
+			goto py_err;
+
+		value = PyString_FromStringAndSize((const char *)&ucp[2], ucp[1]);
+		if (!value) {
+			Py_DECREF(name);
+			goto py_err;
+		}
+
+		if (PyDict_SetItem(dict, name, value) < 0) {
+			Py_DECREF(name);
+			Py_DECREF(value);
+			goto py_err;
+		}
+
+		Py_DECREF(name);
+		Py_DECREF(value);
+
+		ucp += mp_len;
+		mode_data_len -= mp_len;
+	}
+
+	return dict;
+
+py_err:
+	Py_XDECREF(dict);
+
+	return NULL;
+}
+
 static PyMethodDef sgutils_methods[] = {
-	{ "readcap",	readcap, METH_VARARGS,
-	readcap_doc},
+	{ "inquiry", spc_inquiry, METH_VARARGS, inquiry_doc},
+	{ "sense", spc_sense, METH_VARARGS, sense_doc},
+	{ "readcap", sbc_readcap, METH_VARARGS, readcap_doc},
 	{ NULL,	     NULL}	   /* sentinel */
 };
-
 
 PyMODINIT_FUNC
 initsgutils(void)
